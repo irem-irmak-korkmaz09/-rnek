9f697a4cfdda7a41f408dec219dc8e351900a40f
irem-irmak-korkmaz09 <iremirmak415@gmail.com>
2025-02-23 17:02:47 +0300

    Update list_dir.py

--- list_dir.py
+++ list_dir.py
@@ -8,16 +8,17 @@ test_dict={
     },   
 }
 
-def f (start_ path search pattern): 
-      file or folders = os.listdir(start_path)
-      for ff in file or folders:
-         new_path = os.path.join(start_path, ff)
-         if os.path.isdir(ff):
-            f(start __path__=new_path, search_pattern=search_pattern)
-         else:
-             #dosya
+def f(start_path,search_pattern):
+    file_or_folder = os.listdir(start_path)
+    for ff in file_or_folder:
+        new_path=os.path.join(start_path,ff)
+        if os.path.isdir(new_path):
+            f(new_path=new_path,search_pattern=search_pattern)
+        else:
+            if search_pattern in ff:
                 print(new_path)
-print(f{start_path=".", search_pattern="*.md"})                
+print(f(start_path=".",search_pattern=".md"))
+             
          
          
          
